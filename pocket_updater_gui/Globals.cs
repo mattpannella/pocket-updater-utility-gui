5a1ab7da130f3869f675ee641ea8ef519e68e6d8
Matt Pannella <mattpannella@gmail.com>
2023-06-03 13:33:28 -0400

    missed file

---
+++ pocket_updater_gui/Globals.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Net.Http;
+
+namespace pannella.analoguepocket.gui;
+
+public class Globals
+{
+    private static Globals instance = null;
+    private static object syncLock = new object();
+    public SettingsManager? SettingsManager { get; set; }
+    public PocketCoreUpdater? Updater { get; set; }
+
+
+    private Globals()
+    {
+
+    }
+
+    public static Globals Instance
+    {
+        get
+        {
+            lock (syncLock)
+            {
+                if (Globals.instance == null)
+                {
+                    Globals.instance = new Globals();
+                }
+
+                return Globals.instance;
+            }
+        }
+    }
+}
